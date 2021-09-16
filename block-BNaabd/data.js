2182fadc78bf358f28ea728489a89cddf9ca163c
Vineet <vineet98love@gmail.com>
2021-09-16 23:06:58 +0530

    update

--- block-BNaabd/data.js
+++ block-BNaabd/data.js
@@ -1,22 +1,30 @@
 var http = require('http');
-var fs = require('fs');
+var qs = require('querystring');
 
 
 var server = http.createServer(handleRequest);
 
-function handleRequest(res,req){
-    var dataFormat = req.headers('content-type');
-     var store ='';
-     req.on('data', (chunk) => {
-         store = store + chunk;
-     });
-
-     req.on('end', () => {
-         res.write(store);
-         res.end();
+function handleRequest(req,res){
+    
+    var store ='';
+    req.on('data', (chunk) => {
+        store = store + chunk;
+    })
+    
+    req.on('end', () => {
+        if(req.method === 'POST' && req.url === '/json'){
+            console.log(store);
+            res.setHeader('Content-Type', "application/json");
+            res.end(store);
+        }
+        if(req.method === 'POST' && req.url === '/form'){
+            console.log(store);
+            var formData = qs.parse(store);
+            res.end(JSON.stringify(formData));
+        }
      })
-
 }
+
 
 server.listen(7000, () => {
     console.log('Server listening on port 7000');
